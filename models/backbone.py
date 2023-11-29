20bbd52f67bc3acb5e25d1bb432e2601aa2a83f0
Kyle Flores <6565728+kyflores@users.noreply.github.com>
2023-11-28 17:00:52 -1000

    Add shufflenet backbone

--- models/backbone.py
+++ models/backbone.py
@@ -15,6 +15,7 @@ from util.misc import NestedTensor, is_main_process
 
 from .position_encoding import build_position_encoding
 
+from torchinfo import summary
 
 class FrozenBatchNorm2d(torch.nn.Module):
     """
@@ -68,6 +69,7 @@ class BackboneBase(nn.Module):
             return_layers = {'layer4': "0"}
         self.body = IntermediateLayerGetter(backbone, return_layers=return_layers)
         self.num_channels = num_channels
+        print(summary(self.body, (1,3,640,640)))
 
     def forward(self, tensor_list: NestedTensor):
         xs = self.body(tensor_list.tensors)
@@ -92,6 +94,52 @@ class Backbone(BackboneBase):
         num_channels = 512 if name in ('resnet18', 'resnet34') else 2048
         super().__init__(backbone, train_backbone, num_channels, return_interm_layers)
 
+class ShufflenetBackbone(nn.Module):
+    def __init__(self, name: str, return_interm_layers: bool):
+        super().__init__()
+
+        # backbone = torchvision.models.shufflenet_v2_x1_0(weights='IMAGENET1K_V1')
+        backbone = getattr(torchvision.models, name)(weights='IMAGENET1K_V1')
+        if name == 'shufflenet_v2_x0_5':
+            self.num_channels = 1024
+        elif name == 'shufflenet_v2_x1_0':
+            self.num_channels = 1024
+        elif name == 'shufflenet_v2_x1_5':
+            self.num_channels = 1024
+        elif name == 'shufflenet_v2_x2_0':
+            self.num_channels = 2048
+
+        if return_interm_layers:
+            return_layers = {"conv1": "0", "maxpool": "1", "stage2": "2", "stage3": "3", "stage4": "4", "conv5": "5"}
+        else:
+            return_layers = {"conv5": "0"}
+
+        self.body = IntermediateLayerGetter(backbone, return_layers=return_layers)
+        print(summary(self.body, (1,3,640,640)))
+
+        # self.conv_out = nn.Conv2d(1024, self.num_channels, kernel_size=1, padding=0, stride=1)
+
+    def forward(self, tensor_list: NestedTensor):
+        xs = self.body(tensor_list.tensors)
+        out: Dict[str, NestedTensor] = {}
+
+        # Copy paste from Shufflenet source code with final class head removed.
+        # xs = self.backbone.conv1(xs)
+        # xs = self.backbone.maxpool(xs)
+        # xs = self.backbone.stage2(xs)
+        # xs = self.backbone.stage3(xs)
+        # xs = self.backbone.stage4(xs)
+        # xs = self.backbone.conv5(xs)
+        # x = x.mean([2, 3])  # globalpool
+        # x = self.fc(x)
+        # xs = self.conv_out(xs)
+
+        for name, x in xs.items():
+            m = tensor_list.mask
+            assert m is not None
+            mask = F.interpolate(m[None].float(), size=x.shape[-2:]).to(torch.bool)[0]
+            out[name] = NestedTensor(x, mask)
+        return out
 
 class Joiner(nn.Sequential):
     def __init__(self, backbone, position_embedding):
@@ -113,7 +161,12 @@ def build_backbone(args):
     position_embedding = build_position_encoding(args)
     train_backbone = args.lr_backbone > 0
     return_interm_layers = args.masks
-    backbone = Backbone(args.backbone, train_backbone, return_interm_layers, args.dilation)
+    if "resnet" in args.backbone:
+        backbone = Backbone(args.backbone, train_backbone, return_interm_layers, args.dilation)
+    elif "shufflenet_v2" in args.backbone:
+        backbone = ShufflenetBackbone(args.backbone, return_interm_layers)
+    else:
+        raise Exception("Unknown backbone {}".format(args.backbone))
     model = Joiner(backbone, position_embedding)
     model.num_channels = backbone.num_channels
     return model
